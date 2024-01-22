2e4adf8adb0fc91cd25872f93865cf4beeaa0c4e
Anush0108 <karhadkar.anushka@gmail.com>
2024-01-22 19:16:52 +0530

    add new page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./Components/Register.jsx";
 import {NextUIProvider} from "@nextui-org/react";
 import Profile from "./Components/Profile.jsx";
+import JobPage from "./Components/JobPage.jsx";
 function App() {
   return (
     <NextUIProvider>
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile/>} />
+        <Route path="/Jobpage" element={<JobPage/>}/>
       </Routes>
     </BrowserRouter>
     </NextUIProvider>
