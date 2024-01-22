a599eddb76c2395baecb68e505e2c51ec14f0466
Raghav-2104 <mundhararaghav16@gmail.com>
2024-01-22 16:52:38 +0530

    Update Profile component UI

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,45 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Card, CardHeader, CardBody, CardFooter, Avatar, Button } from "@nextui-org/react";
-import {getAuth ,onAuthStateChanged} from "firebase/auth";
+import { Card, CardHeader, CardBody, CardFooter, Avatar, Button, Divider } from "@nextui-org/react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
-// import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link , NavbarMenuItem ,NavbarMenu ,NavbarMenuToggle} from "@nextui-org/react";
-// import {AcmeLogo} from "./AcmeLogo.jsx";
+import { UserIcon } from './UserIcon';
+import { Chip } from '@nextui-org/react';
+import { Textarea } from '@nextui-org/react';
 const ProfileContainer = styled.div`
-    text-align: center;
-    padding: 20px;
-    margin: auto;
-    border: 1px solid #007bff;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: black;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  margin: auto;
+  background-color: black;
+`;
+
+const Header = styled.header`
+  padding: 20px;
+  text-align: right;
+`;
+
+const ContentContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 50px;
+  margin-right: 50px;
 `;
 
 const Profile = () => {
   const [userEmail, setUserEmail] = useState(null);
-  const [userName , setUserName] = useState(null);
-  const [userNumber , setUserNumber] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [userNumber, setUserNumber] = useState(null);
   let navigate = useNavigate();
   const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setUserEmail(user.email);
-      setUserName(user.displayName);
-      setUserNumber(user.phoneNumber);
-    } else {
-      setUserEmail(null);
-    }
-  });
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserEmail(user.email);
+        setUserName(user.displayName);
+        setUserNumber(user.phoneNumber);
+      } else {
+        setUserEmail(null);
+      }
+    });
+    return () => unsubscribe();
+  }, [auth]);
 
   return (
     <ProfileContainer>
-      <header>
+      <Header>
         <Button
           color="primary"
           auto
+          endContent={<UserIcon size={24} />}
           onClick={() => {
             auth.signOut();
             navigate("/");
@@ -47,22 +62,118 @@ const Profile = () => {
         >
           Logout
         </Button>
-      </header>
-      <Card>
-        <CardHeader>
-          <Avatar size="large" src="https://i.imgur.com/1Qd0R2D.jpg" />
-          <p>{userName}</p>
-        </CardHeader>
-        <CardBody>
-          <p>Email: {userEmail}</p>
-          <p>Phone Number: {userNumber}</p>
-        </CardBody>
-        <CardFooter>
-          <Button color="success">Edit Profile</Button>
-        </CardFooter>
-      </Card>
+      </Header>
+      <ContentContainer>
+        <Card
+          className="min-w-full max-w-2xl"
+        >
+          <CardHeader>
+            <Avatar size="large" src="https://i.imgur.com/1Qd0R2D.jpg" />
+            <ul style={{
+              marginLeft: '20px',
+            }}>
+              <li
+                style={{
+                  fontWeight: 'bold',
+                }}
+              >{userName}</li>
+              <li
+                style={{
+                  fontWeight: 'bold',
+                }}
+              >{userEmail}</li>
+            </ul>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <Card
+              className='max-w-fit'
+            >
+              <CardHeader
+                style={{
+                  fontWeight: 'bold',
+                }}
+              >Skills</CardHeader>
+              <CardBody
+                style={{
+                  display: 'inline',
+                }}
+              >
+                <Chip color="danger" style={{
+                  marginRight: '10px',
+                }}
+                  variant='shadow'
+                >
+                  C++
+                </Chip>
+                <Chip color="primary" style={{
+                  marginRight: '10px',
+                }}
+                  variant='shadow'
+                >
+                  Python
+                </Chip>
+                <Chip color="secondary" style={{
+                  marginRight: '10px',
+                }}
+                  variant='shadow'
+                >
+                  Flutter
+                </Chip>
+              </CardBody>
+            </Card>
+            <Divider
+              style={{
+                marginTop: '20px',
+                marginBottom: '20px',
+              }}
+            />
+            <Card>
+              <CardHeader
+                style={{
+                  fontWeight: 'bold',
+                }}
+              >Projects</CardHeader>
+              <CardBody>
+                <ul>
+                  <Textarea
+                    isReadOnly
+                    label="Project 1"
+                    variant="bordered"
+                    labelPlacement="outside"
+                    defaultValue="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+                    className="max-w-5xl"
+                  />
+                  <br />
+                  <Textarea
+                    isReadOnly
+                    label="Project 2"
+                    variant="bordered"
+                    labelPlacement="outside"
+                    defaultValue="Give a brief description of your project here."
+                    className="max-w-5xl"
+                  />
+                  <br />
+                  <Textarea
+                    isReadOnly
+                    label="Project 3"
+                    variant="bordered"
+                    labelPlacement="outside"
+                    defaultValue="Give a brief description of your project here."
+                    className="max-w-5xl"
+                  />
+
+                </ul>
+              </CardBody>
+            </Card>
+          </CardBody>
+          <CardFooter>
+            <Button color="success">Edit Profile</Button>
+          </CardFooter>
+        </Card>
+      </ContentContainer>
+      <br />
     </ProfileContainer>
-    
   );
 };
 
