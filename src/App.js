5db817a943214919cb895389e673f0261d10dcb7
Raghav-2104 <mundhararaghav16@gmail.com>
2024-03-04 10:32:24 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import Login from "./Components/Login.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./Components/Register.jsx";
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 import Profile from "./Components/Profile.jsx";
 import JobPage from "./Components/JobPage.jsx";
 import JobListing from "./Components/JobListing.jsx";
 import PostJob from "./Components/PostJob.jsx";
 import EditProfile from "./Components/EditProfile.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
-    <NextUIProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/Jobpage/:id" element={<JobPage/>}/>
-        <Route path="/editprofile" element={<EditProfile/>} />
-        <Route path="/joblisting" element={<JobListing/>} />
-        <Route path="/postjob" element={<PostJob/>} />  
-      </Routes>
-    </BrowserRouter>
-    </NextUIProvider>
+    <div>
+      <SpeedInsights />
+      <NextUIProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/Jobpage/:id" element={<JobPage />} />
+            <Route path="/editprofile" element={<EditProfile />} />
+            <Route path="/joblisting" element={<JobListing />} />
+            <Route path="/postjob" element={<PostJob />} />
+          </Routes>
+        </BrowserRouter>
+      </NextUIProvider>
+    </div>
   );
 }
 
