c491bb2dab1e8f824bb1003abab33882212276b0
Raghav-2104 <mundhararaghav16@gmail.com>
2024-03-24 15:50:34 +0530

    Reviews Pending

---
+++ src/Components/Review.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    text-align: center;
+    padding: 20px;
+    margin: auto;
+    // border: 1px solid #007bff;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+`;
+function Review() {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Review
